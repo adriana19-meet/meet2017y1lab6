155a909a86e53337f9bff14818034c14c7862ad3
adriana19-meet <adriana19@meet.mit.edu>
2017-07-31 18:29:49 +0300

    Stopping here

--- funturtle.py
+++ funturtle.py
@@ -1,18 +1,30 @@
-
-def up():
-def down()
-def left()
-def right()
-
 UP_ARROW = "Up"
 LEFT_ARROW = "Left"
 DOWN_ARROW = "Down"
 RIGH_ARROW = "Right"
 SPACEBAR = "space"
 UP = 0
+LEFT = 1
+DOWN = 2
+RIGHT = 3
 
-direction = UP 
+direction = DOWN
 
+def up():
+    global direction
+    direction = UP
+    
+def down():
+    
+    global direction
+    direction = DOWN
+    
+def left():
+    global direction
+    direction = LEFT
+    
+def right():
+    global direction 
+    direction = RIGHT
 
-
-
+print (UP)
