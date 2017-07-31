a07cfd094668d4a458c9ddfa0153d31dcf2661af
adriana19-meet <adriana19@meet.mit.edu>
2017-07-31 17:21:04 +0300

    Stopping here

---
+++ funturtle.py
@@ -0,0 +1,18 @@
+
+def up():
+def down()
+def left()
+def right()
+
+UP_ARROW = "Up"
+LEFT_ARROW = "Left"
+DOWN_ARROW = "Down"
+RIGH_ARROW = "Right"
+SPACEBAR = "space"
+UP = 0
+
+direction = UP 
+
+
+
+
